e75fb96e0a407b0662105b6e7b6fb1c4b2f159b8
Kayle <i493302@fhict.nl>
2022-12-09 14:36:59 +0100

    Initial commit v2

---
+++ src/pages/Register.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState, useContext } from "react";
+import axios from "../api/axios";
+ 
+const Register = () => {
+    //Make form push to backend
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const errorRef = useRef();
+
+    const REGISTER_URL = "/api/register"
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        emailRef.current.focus();
+    }, [])
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await axios.post(REGISTER_URL, JSON.stringify({email, password}), 
+                {
+                    headers: { 'Content-Type': 'application/json'},
+                    withCredentials: false
+                }
+            );
+            window.location = "/"
+        }catch(error){
+            if(!error.response){
+                setError("Database could not be reached");
+                console.log(error);
+            }else if (error.response?.status === 500){
+                setError("no server connection was made")
+                console.log("no server connection was made");
+            }else if (error.response?.status === 403){
+                setError("Email already in use")
+            }
+        }
+
+    } 
+
+    return (
+        <>
+        <div>
+            <p ref={errorRef}> {error}</p>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" value={email} ref={emailRef} onChange={(e) => setEmail(e.target.value)} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" value={password} ref={passwordRef} onChange={(e) => setPassword(e.target.value)} id="exampleInputPassword1" placeholder="Password"></input>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+        </div>
+       </>
+    );
+}
+ 
+export default Register;
+
---
+++ src/pages/User/Users.jsx
@@ -0,0 +1,40 @@
+import React, {Component, useEffect, useRef, useState} from 'react';
+import axios from "../../api/axios";
+
+const Users = () => {
+    //Make form push to backend
+    const errorRef = useRef();
+    const [error, setError] = useState('');
+    const [users, setUsers] = useState('');
+    const USERS_URL = "/api/user"
+
+    useEffect( () => {
+        getUser();
+    }, [])
+
+    async function getUser(){
+        try{
+            const response = await axios.get(USERS_URL);
+            setUsers(response.data);
+            console.log("Get users succes")
+        }catch(error){
+            console.log("Get users failed")
+        }
+
+    }
+
+
+    return (
+        <div>
+            <h1>Users</h1>
+            { Array.isArray(users) ? users.map(user =>
+                <li key={user.id}>
+                    <a href={"user/"+user.id}>{user.id} - {user.email}</a>
+                </li>
+
+            ) : null}
+        </div>
+    );
+}
+
+export default Users;
